7cd2f0f0493f7550d92dbf7ea9b6aa1804679648
Shadab Khan <khan.shadab338@gmail.com>
2026-02-24 14:54:57 +0530

    changed test class name

--- src/test/java/com/eduproject/EduProApplicationTests.java
+++ src/test/java/com/eduproject/EduProApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SbMvc06SimpleCrudEduProApplicationTests {
+class EduProApplicationTests {
 
 	@Test
 	void contextLoads() {
